6a00687b8915970817e6dd7a8583df2bb4c0fa43
hyukhur <hyukhur@gmail.com>
2014-04-11 00:43:06 +0900

    Delegate test skeleton

--- HHFixedResultsControllerTests/HHFetchedResultControllerDelegateTest.m
+++ HHFixedResultsControllerTests/HHFetchedResultControllerDelegateTest.m
@@ -7,9 +7,13 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "HHFixedResultsController.h"
+#import <CoreData/CoreData.h>
+
 
 @interface HHFetchedResultControllerDelegateTest : XCTestCase
-
+@property (nonatomic, strong)HHFixedResultsController *frc;
+@property (nonatomic, strong)NSArray *objects;
 @end
 
 @implementation HHFetchedResultControllerDelegateTest
@@ -17,7 +21,24 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here; it will be run once, before the first test case.
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:nil];
+    request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"detail" ascending:YES]];
+    request.predicate = [NSPredicate predicateWithFormat:@"title != %@", @"title"];
+    
+    self.objects = @[
+                     @{@"type":@"1type", @"title":@"title one", @"detail":@"test value1", @"type2":@""},
+                     @{@"type":@"2type", @"title":@"title two", @"detail":@"test value2", @"type2":@""},
+                     @{@"type":@"1types",@"title":@"title fouth", @"detail":@"test value4", @"type2":@""},
+                     @{@"type":@"1type", @"title":@"title zero", @"detail":@"test value0", @"type2":@""},
+                     @{@"type":@"1type", @"title":@"title", @"detail":@"test value0", @"type2":@""},
+                     ];
+    
+    self.frc = [[HHFixedResultsController alloc]
+                initWithFetchRequest:request
+                objects:self.objects
+                sectionNameKeyPath:@"type"
+                cacheName:nil];
+    [self.frc performFetch:nil];
 }
 
 - (void)tearDown
@@ -26,9 +47,50 @@
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+//- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath;
+- (void) testDidChangeObjectWithNSFetchedResultsChangeInsert {
+    
+}
+
+//- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath;
+- (void) testDidChangeObjectWithNSFetchedResultsChangeDelete {
+    
+}
+
+//- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath;
+- (void) testDidChangeObjectWithNSFetchedResultsChangeMove {
+    
+}
+
+//- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath;
+- (void) testDidChangeObjectWithNSFetchedResultsChangeUpdate {
+    
+}
+
+//- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type;
+- (void) testDidChangeSectionWithNSFetchedResultsChangeInsert {
+    
+}
+
+//- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type;
+- (void) testDidChangeSectionWithNSFetchedResultsChangeDelete {
+
+}
+
+
+//- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller;
+- (void) testWillChangeContent {
+    
+}
+
+//- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller;
+- (void) testDidChangeContent {
+    
+}
+
+//- (NSString *)controller:(NSFetchedResultsController *)controller sectionIndexTitleForSectionName:(NSString *)sectionName __OSX_AVAILABLE_STARTING(__MAC_NA,__IPHONE_4_0);
+- (void) testSectionIndexTitleForSectionName {
+    
 }
 
 @end
